9ef519236d4490fc513bf334b644ef0cf6412956
ExoPrisma <SMiHuang02084@gmail.com>
2024-06-04 23:29:33 -0400

    Created util.js Added isInt function

---
+++ script/utils.js
@@ -0,0 +1,13 @@
+/**
+ * Javascript containing utility functions
+ */
+
+/**
+ * Test if number is integer
+ * 
+ * @param {number} num number to be tested
+ * @returns true if num is an integer
+ */
+function isInt(num) {
+  return num === Math.floor(num);
+}
