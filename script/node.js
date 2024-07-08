3c349e49f52489e6caf3621f0a26a4ad2f26b4b8
ExoPrisma <SMiHuang02084@gmail.com>
2024-07-07 21:59:28 -0400

    Added nextGeneration(), center(), getZero()

--- script/node.js
+++ script/node.js
@@ -44,6 +44,11 @@ class Node {
     max: 1024, 
   });
 
+  // Cache for future generations
+  static #cacheSuccessor = new LRUCache({
+    max : Math.pow(2, 24), 
+  })
+
   /**
    * Takes a number of parameters to create a Node.
    * @param {number} lvl lvl of the node
@@ -70,6 +75,11 @@ class Node {
 
   /**
    * Display the content of a node to a grid.
+   * Each cell is separated by an "-" vertically and a "|" horizontally. 
+   *    Example:
+   *          0 | 0 
+   *          - - -
+   *          0 | 0
    */
   toGrid() {
     if(this.#lvl == 0){
@@ -88,8 +98,6 @@ class Node {
 
       Node.#print(grid);
     }
-
-    
   }
   /**
    * Helper recursive method fill grid.
@@ -196,28 +204,163 @@ class Node {
    * @returns new node has value
    */
   static #computeHash(nwChild, neChild, swChild, seChild){
-    const combined = `${nwChild.k}${nwChild.#hash}${neChild.#hash}${swChild.#hash}${seChild.#hash}`;
+    const combined = `${nwChild.#lvl}${nwChild.#hash}${neChild.#hash}${swChild.#hash}${seChild.#hash}`;
     const hashValue = murmurhash.v3(combined);
     return BigInt(hashValue) & ((1n << 63n) - 1n);
   }
 
   /**
-   * returns an empty node.
+   * Returns an empty node.
    * 
    * @param {number} lvl of the empty node
    * @returns empty node
    */
   static get_zero(lvl){
-    if(lvl == 0){
-      return OFF
+    const cacheKey = `${lvl}`;
+    
+    if (Node.#cacheEmpty.has(cacheKey)) {
+      return Node.#cacheEmpty.get(cacheKey);
+    }
+
+    const kZeroNode = lvl == 0 ? OFF : Node.join(Node.get_zero(lvl - 1), 
+                                                 Node.get_zero(lvl - 1), 
+                                                 Node.get_zero(lvl - 1), 
+                                                 Node.get_zero(lvl - 1));
+
+    Node.#cacheEmpty.set(cacheKey, kZeroNode);
+      return kZeroNode;
+  }
+
+  /**
+   * Returns new node with node in the middle
+   *    Example: 
+   *                   0 | 0 | 0 | 0
+   *                   - - - - - - -
+   *          a | b    0 | a | b | 0
+   *          - - - => - - - - - - -
+   *          c | d    0 | c | d | 0
+   *                   - - - - - - -
+   *                   0 | 0 | 0 | 0
+   * 
+   * @param {Node} node at the center
+   * @returns new node with node in the middle
+   */
+  static centre(node){
+    const emptyNode = Node.get_zero(node.#lvl - 1);
+    const centerNode = join(
+      join(emptyNode, emptyNode, emptyNode, node.#children[0]), 
+      join(emptyNode, emptyNode, node.#children[1], emptyNode),
+      join(emptyNode, node.#children[2], emptyNode, emptyNode), 
+      join(node.#children[3], emptyNode, emptyNode, emptyNode)
+    )
+
+    return centerNode;
+  }
+
+  /**
+   * Return the 2^{lvl-1} x 2^{lvl-1} successor, 2^{skip} generations in the future
+   * 
+   * @param {Node} node to calculate next generations
+   * @param {number} skip number of generation in the future
+   * @returns successor of node
+   */
+  static nextGeneration(node, skip=null){
+    let nextGenNode;
+    
+    const cacheKey = `${node.#children[0].#hash}-${node.#children[1].#hash}-${node.#children[2].#hash}-${node.#children[3].#hash}`;
+    
+    if (Node.#cacheSuccessor.has(cacheKey)) {
+      return Node.#cacheSuccessor.get(cacheKey);
+    }
+
+    if(node.#numCells == 0){
+      nextGenNode = node.#children[0];
+    }
+    else if(node.#numCells == 2){
+      nextGenNode = Node.#life4x4(node);
     }
     else{
-      return Node.join(Node.get_zero(lvl - 1), 
-                       Node.get_zero(lvl - 1), 
-                       Node.get_zero(lvl - 1), 
-                       Node.get_zero(lvl - 1));
+      skip = (skip == null) ? node.#lvl - 2 : Math.min(skip, node.#lvl - 2);
+
+      const nwNode = nextGeneration(join(node.#children[0].#children[0], node.#children[0].#children[1], node.#children[0].#children[2], node.#children[0].#children[3]), skip);
+      const nNode = nextGeneration(join(node.#children[0].#children[1], node.#children[1].#children[0], node.#children[0].#children[3], node.#children[1].#children[2]), skip);
+      const neNode = nextGeneration(join(node.#children[1].#children[0], node.#children[1].#children[1], node.#children[1].#children[2], node.#children[1].#children[3]), skip);
+      const wNode = nextGeneration(join(node.#children[0].#children[2], node.#children[0].#children[3], node.#children[2].#children[0], node.#children[2].#children[1]), skip);
+      const centerNode = nextGeneration(join(node.#children[0].#children[3], node.#children[1].#children[2], node.#children[2].#children[1], node.#children[3].#children[0]), skip);
+      const eNode = nextGeneration(join(node.#children[1].#children[2], node.#children[1].#children[3], node.#children[3].#children[0], node.#children[3].#children[1]), skip);
+      const swNode = nextGeneration(join(node.#children[2].#children[0], node.#children[2].#children[1], node.#children[2].#children[2], node.#children[2].#children[3]), skip);
+      const sNode = nextGeneration(join(node.#children[2].#children[1], node.#children[3].#children[0], node.#children[2].#children[3], node.#children[3].#children[2]), skip);
+      const seNode = nextGeneration(join(node.#children[3].children[0], node.#children[3].#children[1], node.#children[3].#children[2], node.#children[3].#children[3]), skip);
+
+      if(skip < m.k - 2){
+        nextGenNode = join(
+          (join(nwNode.#children[3], nNode.#children[2], wNode.#children[1], centerNode.#children[0])),
+          (join(nNode.#children[3], neNode.#children[2], centerNode.#children[1], eNode.#children[0])),
+          (join(wNode.#children[3], centerNode.#children[2], swNode.#children[1], sNode.#children[0])),
+          (join(centerNode.#children[3], eNode.#children[2], sNode.#children[1], seNode.#children[0])));
+      }
+      else{
+        nextGenNode = join(
+          nextGeneration(join(nwNode, nNode, wNode, centerNode), skip),
+          nextGeneration(join(nNode, neNode, centerNode, swNode), skip),
+          nextGeneration(join(wNode, centerNode, eNode, sNode), skip),
+          nextGeneration(join(centerNode, swNode, sNode, seNode), skip));
+      }
     }
     
+    Node.#cacheSuccessor.set(cacheKey, nextGenNode)
+
+    return nextGenNode;
+  }
+  /**
+   * Base case, returns next generation of 4x4 nodes
+   * 
+   * @param {Node} node 4x4 
+   * @returns 4x4 node for next generation
+   */
+  static #life4x4(node){
+    assertCondition(node.#lvl == 2, "Node is not a 4x4 node", node.#lvl);
+
+    const nwIndices = [[0, 0], [0, 1], [1, 0], [0, 2], [0, 4], [1, 2], [2, 0], [2, 1], [3, 0]];
+    const neIndices = [[0, 1], [1, 0], [1, 1], [0, 3], [1, 2], [1, 3], [2, 1], [3, 0], [3, 1]];
+    const swIndices = [[0, 2], [0, 3], [1, 2], [2, 0], [2, 1], [3, 0], [2, 2], [2, 3], [3, 2]];
+    const seIndices = [[0, 3], [1, 2], [1, 3], [2, 1], [3, 0], [3, 1], [2, 3], [3, 2], [3, 3]];
+
+    function getChildren(indices) {
+      return indices.map(index => node.#children[index[0]].#children[index[1]]);
+    }
+
+    const nwChild = Node.#lifeRule(...getChildren(nwIndices));
+    const neChild = Node.#lifeRule(...getChildren(neIndices));
+    const swChild = Node.#lifeRule(...getChildren(swIndices));
+    const seChild = Node.#lifeRule(...getChildren(seIndices));
+
+    return join(nwChild, neChild, swChild, seChild);
+  }
+  /**
+   * Calculated the outcome of cell in next generation using its surrounding
+   * Currently implementing the basic Game of life rules:
+   *    - A live cell dies if it has fewer than two live neighbors.
+   *    - A live cell with two or three live neighbors lives on to the next generation.
+   *    - A live cell with more than three live neighbors dies.
+   *    - A dead cell will be brought back to live if it has exactly three live neighbors.
+   * (Source: https://beltoforion.de/en/game_of_life/)
+   * 
+   * @param {Node} nw NorthWest node from node
+   * @param {Node} n North node from node
+   * @param {Node} ne NorthEast node from node
+   * @param {Node} e East node from node
+   * @param {Node} node that might be changing in the next generation
+   * @param {Node} w West node from node
+   * @param {Node} sw SouthWest node from node
+   * @param {Node} s South node from node
+   * @param {Node} se SouthEast node from node
+   * @returns The outcome of node for next generation
+   */
+  static #lifeRule(nw, n, ne, e, node, w, sw, s, se){
+    const sumOuterGrid = [nw, n, ne, e, w, sw, s, se].reduce((sumCell, currentNode) => sumCell + currentNode.#numCells, 0);
+
+    return (node.#numCells && sumOuterGrid == 2) || sumOuterGrid == 3 ? ON : OFF;
   }
 }
 
